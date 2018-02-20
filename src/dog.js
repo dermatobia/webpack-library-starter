c3bfc4a56e3b6574891c2f0d2a0dce2dfe3de5d6
dermatobia <webblewebble@gmail.com>
2018-02-19 19:31:50 -0500

    test console log

--- src/dog.js
+++ src/dog.js
@@ -1,6 +1,7 @@
 export default class Dog {
   constructor() {
     this._name = 'Dog';
+    console.log('in dog class');
   }
   get name() {
     return this._name;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import Cat from './cat.js';
 import Dog from './dog.js';
+console.log('in index.js', Cat);
 export { Dog, Cat };
